ed599eae887eae43565227edf94dc2daab489eb8
Denno <dellyit001@gmail.com>
2024-01-28 11:59:29 -0800

    additional codes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -208,6 +208,26 @@ class HomeController extends Controller
 
     }
 
+    public function sendSms(string $number, string $message)
+    {
+        $username = 'dennohosi'; // use 'sandbox' for development in the test environment
+        $apiKey   =  getenv('AT_API_KEY'); // use your sandbox app API key for development in the test environment
+        $AT       = new AfricasTalking($username, $apiKey);
 
 
-}
+        // Get one of the services
+        $sms      = $AT->sms();
+
+
+        // Use the service
+        $result   = $sms->send([
+            'to'      => $number,
+            'message' => $message,
+        ]);
+
+
+
+
+
+
+    }
