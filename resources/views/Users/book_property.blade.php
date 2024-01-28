46d5a8887b35668ba3d41c30d783771de63dffaa
Denno <dellyit001@gmail.com>
2024-01-28 11:55:31 -0800

    additional codes

--- resources/views/Users/book_property.blade.php
+++ resources/views/Users/book_property.blade.php
@@ -185,15 +185,6 @@
 
 
 
-
-            <!-- Add this div to your HTML to display the rooms -->
-
-
-
-
-
-
-
             <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
             <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
