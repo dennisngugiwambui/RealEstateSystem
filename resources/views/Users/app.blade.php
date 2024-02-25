6e5ee1f62ffa2c87c618967729d432953c9ce597
Denno <dellyit001@gmail.com>
2024-02-24 20:14:17 -0800

    view file

--- resources/views/Users/app.blade.php
+++ resources/views/Users/app.blade.php
@@ -125,7 +125,7 @@
 									<a href="#" class="list-group-item">
 										<div class="row g-0 align-items-center">
 											<div class="col-2">
-												<img src="img/avatars/avatar-5.jpg" class="avatar img-fluid rounded-circle" alt="Vanessa Tucker">
+												<img src="{{asset('User/img/avatars/avatar-5.jpg')}}" class="avatar img-fluid rounded-circle" alt="Vanessa Tucker">
 											</div>
 											<div class="col-10 ps-2">
 												<div class="text-dark">Vanessa Tucker</div>
